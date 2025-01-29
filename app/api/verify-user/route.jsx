adea91c94a6ead5f8e1201a2057c4ea39a16a2c0
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2025-01-29 22:21:43 +0500

    verified-user successfully without issues

--- app/api/verify-user/route.jsx
+++ app/api/verify-user/route.jsx
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
-import {db} from "@/config/db"
+import { db } from "../../../config/db";
 import {eq} from "drizzle-orm"
-import { Users } from "@/config/schema"
+import { Users } from "../../../config/schema"
 
 export async function POST (req) {
     const requestData = await req.json();
@@ -20,8 +20,8 @@ export async function POST (req) {
             const SaveResult = await db.insert(Users).values({
                 name: user?.fullName,
                 email: user?.primaryEmailAddress?.emailAddress,
-                image: user?.imageUrl,
-            }).returning({Users})
+                imageUrl: user?.imageUrl,
+            }).returning({Users});
 
             return NextResponse.json({'result': SaveResult[0]})
         }
