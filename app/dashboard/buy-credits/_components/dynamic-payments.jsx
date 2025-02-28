a5849c86cf995e8165afcfa6ff03148cb5d82738
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2025-02-28 17:39:38 +0500

    update gitignore

--- app/dashboard/buy-credits/_components/dynamic-payments.jsx
+++ app/dashboard/buy-credits/_components/dynamic-payments.jsx
@@ -95,7 +95,7 @@ function DynamicPayments({ selectedOption }) {
             className="bg-blue-500 text-white px-4 py-2 rounded-md"
             onClick={handleCheckout}
         >
-            Proceed to Checkout
+            Checkout
         </button>
     );
 }
--- app/dashboard/buy-credits/page.jsx
+++ app/dashboard/buy-credits/page.jsx
@@ -15,8 +15,8 @@ function BuyCredits() {
   const [selectedOption, setSelectedOption] = useState(null);
 
   return (
-    <div className="neon-effect p-6 bg-white dark:bg-black rounded-lg shadow-lg">
-      <div className="max-w-4xl mx-auto p-6 bg-white dark:bg-black rounded-lg shadow-lg">
+    <div className="neon-effect mb-10 bg-white dark:bg-black rounded-lg shadow-lg">
+      <div className="max-w-4xl mx-auto p-10 bg-white dark:bg-black rounded-lg shadow-lg">
         <h2 className="text-3xl font-bold text-center mb-2 dark:text-white">Buy More Credits</h2>
         <p className="text-gray-600 text-center mb-6">
           Unlock endless possibilities – Buy more credits and transform your room into a masterpiece.
